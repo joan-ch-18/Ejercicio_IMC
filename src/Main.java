d405d635bcef78ffc9f861917dfd96f8d93b8687
joan-ch-18 <joanchindoyh@gmail.com>
2025-03-10 09:25:37 -0500

    Quiz Conversiones

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,152 @@
-import java.util.Random;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int opc, producto, c1=0, c2=0, c3=0, c4=0, c5=0;
-        String p1 = "Mouse", p2 = "Teclados", p3 = "Monitores", p4 = "Discos Duros", p5 = "USB";
-        String nombre, celuco;
-        double valorCompra1=0, valorCompra2=0, valorCompra3=0, valorCompra4=0, valorCompra5=0;
-        double v1=85000, v2= 230000, v3= 940000, v4= 220000, v5= 60000;
+        int opc;
+        double C, F, K, R, Re, operar;
         boolean cambio = true;
-
-        System.out.println("    Dígite su nombre para continuar: ");
-        nombre = teclado.next();
-        System.out.println("    Ahora dígite su número de telefono: ");
-        celuco = teclado.next();
-
         do{
+            System.out.println("    * * * MENÚ DE CONVERSIONES * * *");
             System.out.println("""
-                    * * * MENÚ TIENDA DE TECNOLOGÍA * * *
-                    1. Comprar.
-                    2. Calcular compra.
-                """);
-            System.out.println("    Dígite la opción que desea elegir: ");
+                        1. Celsius a Fahrenheit.
+                        2. Celsius a Kelvin.
+                        3. Celsius a Rankine.
+                        4. Celsius a Réaumur.
+                        5. Fahrenheit a Celsius.
+                        6. Fahrenheit a Kelvin.
+                        7. Fahrenheit a Rankine.
+                        8. Fahrenheit a Réaumur.
+                        9. Kelvin a Celsius.
+                        10. Kelvin a Fahrenheit.
+                        11. Kelvin a Rankine.
+                        12. Kelvin a Réaumur.
+                        13. Rankine a Celsius.
+                        14. Rankine a Fahrenheit.
+                        15. Rankine a Kelvin.
+                        16. Rankine a Réaumur.
+                        17. Salir.
+                    """);
+            System.out.println("    Dígite la opción que desea realizar: ");
             opc = teclado.nextInt();
-
             switch (opc){
                 case 1 -> {
-                    System.out.println("""
-                            + + + COMPRAR PRODUCTOS + + +
-                               Producto   |  Valor
-                            1. Mouse      | $85.000
-                            2. Teclado    | $230.000
-                            3. Monitor    | $940.000
-                            4. Disco Duro | $220.000
-                            5. USB        | $60.000
-                        """);
-                System.out.println("    Dígite el número del producto que desea comprar: ");
-                producto = teclado.nextInt();
-                if(producto == 1){
-                    System.out.println("    ¿Cuántos "+p1+" desea comprar?");
-                    c1 = teclado.nextInt();
-                    valorCompra1 = c1 * v1;
-                } else if (producto == 2) {
-                    System.out.println("    ¿Cuántos "+p2+" desea comprar?");
-                    c2 = teclado.nextInt();
-                    valorCompra2 = c2 * v2;
-                } else if (producto == 3) {
-                    System.out.println("    ¿Cuántos "+p3+" desea comprar?");
-                    c3 = teclado.nextInt();
-                    valorCompra3 = c3 * v3;
-                } else if (producto == 4) {
-                    System.out.println("    ¿Cuántos "+p4+" desea comprar?");
-                    c4 = teclado.nextInt();
-                    valorCompra4 = c4 * v4;
-                } else if (producto == 5) {
-                    System.out.println("    ¿Cuántas "+p5+" desea comprar?");
-                    c5 = teclado.nextInt();
-                    valorCompra5 = c5 * v5;
-                }
+                    System.out.println("    Dígite el número de grados Celsius: ");
+                    C = teclado.nextDouble();
+                    operar = (C*1.8)+32;
+                    System.out.println("    "+C+"° Celsius a Fahrenheit es: "+operar+"°");
+                    System.out.println("");
                 }
                 case 2 -> {
-                    double valorTotal=(valorCompra1+valorCompra2+valorCompra3+valorCompra4+valorCompra5);
-                    double iva = 0.16;
-                    double valorT=(valorTotal+(valorTotal*iva));
-                    System.out.println("    - - - - FACTURA DE VENTA - - - -");
-                    System.out.println("    Nombre: "+nombre+"  Telefono: "+celuco);
-                    System.out.println("    Producto | Cantidad | v/unit | v/total");
-                    System.out.println("    "+p1+" | "+c1+" | "+v1+" | "+valorCompra1);
-                    System.out.println("    "+p2+" | "+c2+" | "+v2+" | "+valorCompra2);
-                    System.out.println("    "+p3+" | "+c3+" | "+v3+" | "+valorCompra3);
-                    System.out.println("    "+p4+" | "+c4+" | "+v4+" | "+valorCompra4);
-                    System.out.println("    "+p5+" | "+c5+" | "+v5+" | "+valorCompra5);
+                    System.out.println("    Dígite el número de grados Celsius: ");
+                    C = teclado.nextDouble();
+                    operar = C+273.15;
+                    System.out.println("    "+C+"° Celsius a Kelvin es: "+operar+"°");
                     System.out.println("");
-                    System.out.println("    V/Total sin Iva: "+valorTotal);
-                    System.out.println("            IVA 16%: "+(valorTotal*iva));
-                    System.out.println("              TOTAL: "+valorT);
+                }
+                case 3 -> {
+                    System.out.println("    Dígite el número de grados Celsius: ");
+                    C = teclado.nextDouble();
+                    operar = (C+273.15)*1.8;
+                    System.out.println("    "+C+"° Celsius a Rankine es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 4 -> {
+                    System.out.println("    Dígite el número de grados Celsius: ");
+                    C = teclado.nextDouble();
+                    operar = C*0.8;
+                    System.out.println("    "+C+"° Celsius a Réaumur es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 5 -> {
+                    System.out.println("    Dígite el número de grados Fahrenheit: ");
+                    F = teclado.nextDouble();
+                    operar = (F-32)*0.55;
+                    System.out.println("    "+F+"° Fahrenheit a Celsius es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 6 -> {
+                    System.out.println("    Dígite el número de grados Fahrenheit: ");
+                    F = teclado.nextDouble();
+                    operar = (F-32)*0.55+273.15;
+                    System.out.println("    "+F+"° Fahrenheit a Kelvin es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 7 -> {
+                    System.out.println("    Dígite el número de grados Fahrenheit: ");
+                    F = teclado.nextDouble();
+                    operar = F+459.67;
+                    System.out.println("    "+F+"° Fahrenheit a Rankine es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 8 -> {
+                    System.out.println("    Dígite el número de grados Fahrenheit: ");
+                    F = teclado.nextDouble();
+                    operar = (F-32)*0.44;
+                    System.out.println("    "+F+"° Fahrenheit a Réaumur es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 9 -> {
+                    System.out.println("    Dígite el número de grados Kelvin: ");
+                    K = teclado.nextDouble();
+                    operar = K - 273.15;
+                    System.out.println("    "+K+"° Kelvin a Celsius es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 10 -> {
+                    System.out.println("    Dígite el número de grados Kelvin: ");
+                    K = teclado.nextDouble();
+                    operar = (K-273.15)*1.8+32;
+                    System.out.println("    "+K+"° Kelvin a Fahrenheit es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 11 -> {
+                    System.out.println("    Dígite el número de grados Kelvin: ");
+                    K = teclado.nextDouble();
+                    operar = K*1.8;
+                    System.out.println("    "+K+"° Kelvin a Rankine es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 12 -> {
+                    System.out.println("    Dígite el número de grados Kelvin: ");
+                    K = teclado.nextDouble();
+                    operar = (K-273.15)*0.8;
+                    System.out.println("    "+K+"° Kelvin a Réaumur es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 13 -> {
+                    System.out.println("    Dígite el número de grados Rankine: ");
+                    R = teclado.nextDouble();
+                    operar = (R-491.67)*0.555;
+                    System.out.println("    "+R+"° Rankine a Celsius es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 14 -> {
+                    System.out.println("    Dígite el número de grados Rankine: ");
+                    R = teclado.nextDouble();
+                    operar = R-459.67;
+                    System.out.println("    "+R+"° Rankine a Fahrenheit es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 15 -> {
+                    System.out.println("    Dígite el número de grados Rankine: ");
+                    R = teclado.nextDouble();
+                    operar = R*0.555;
+                    System.out.println("    "+R+"° Rankine a Kelvin es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 16 -> {
+                    System.out.println("    Dígite el número de grados Rankine: ");
+                    R = teclado.nextDouble();
+                    operar = (R-491.67)*0.444;
+                    System.out.println("    "+R+"° Rankine a Réaumur es: "+operar+"°");
+                    System.out.println("");
+                }
+                case 17 ->{
                     cambio = false;
                 }
             }
-        }while(cambio);
+        }while (cambio);
+
 
     }
 }
